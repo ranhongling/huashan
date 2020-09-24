96786454a2c6e87db6a53b1f81caa5cc42455831
tom_glb <goodMorning_glb@atguigu.com>
2020-09-24 16:40:50 +0800

    添加文件

---
+++ helloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author: ranhongling
+ * @date ：Created in 2020/9/24
+ * @description ：
+ * @version: 1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
